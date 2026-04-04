374ff022ee9049ab1dcb96dc1d7c939106eb17c4
Yuichiro Hayashi <onihei_99@yahoo.co.jp>
2026-04-04 14:16:44 +0900

    BBSのリファクタ

---
+++ lib/ui/pages/bbs/bbs_controller.dart
@@ -0,0 +1,150 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:greendayo/domain/model/profile.dart';
+import 'package:greendayo/domain/model/user.dart';
+import 'package:greendayo/entity/article.dart';
+import 'package:greendayo/repository/article_repository.dart';
+import 'package:greendayo/ui/pages/bbs/bbs_form.dart';
+import 'package:greendayo/ui/utils/snackbars.dart';
+import 'package:greendayo/web/my_image_utils.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:image/image.dart' as img;
+import 'package:image_picker/image_picker.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'bbs_controller.g.dart';
+
+@riverpod
+class BbsController extends _$BbsController {
+  final formContainerKey = GlobalKey();
+  final formKey = GlobalKey();
+
+  @override
+  BbsController build() {
+    return this;
+  }
+
+  void startCreate() {
+    ref.read(bbsScaleProvider.notifier).setScale(1.0);
+    ref.read(bbsFormEnabledProvider.notifier).setEnabled(true);
+  }
+
+  Future<void> post(BuildContext context) async {
+    final myProfile = ref.read(myProfileProvider).requireValue;
+
+    final stackBox =
+        formContainerKey.currentContext?.findRenderObject() as RenderBox;
+    final formBox = formKey.currentContext?.findRenderObject() as RenderBox;
+    final positionStack = stackBox.localToGlobal(Offset.zero);
+    final positionForm = formBox.localToGlobal(Offset.zero);
+    final formOffset = Offset(
+      positionForm.dx - positionStack.dx,
+      positionForm.dy - positionStack.dy,
+    );
+
+    String text;
+    final form = ref.read(bbsFormProvider);
+    final screenOffset = ref.read(bbsOffsetProvider);
+
+    if (form.isPhotoMode) {
+      final url = await ref
+          .read(articleRepositoryProvider)
+          .uploadJpeg(form.photoJpgBytes!);
+      text = '<img src="$url" data-rotation="${form.rotation}" />';
+    } else {
+      text = form.text;
+      if (text.trim().isEmpty) {
+        showSnackBar(
+          context,
+          ref,
+          content: Text('投稿内容を記入してください'),
+        );
+        return;
+      }
+    }
+    final now = DateTime.now();
+    final left = -screenOffset.dx + formOffset.dx;
+    final top = -screenOffset.dy + formOffset.dy;
+    final newArticle = Article(
+      content: text,
+      signature: myProfile.nickname,
+      left: left,
+      top: top,
+      width: form.width,
+      createdAt: now,
+      createdBy: myProfile.userId,
+    );
+    final docId = await ref.read(articleRepositoryProvider).save(newArticle);
+    ref.read(bbsFormEnabledProvider.notifier).setEnabled(false);
+    ref.read(bbsFormProvider.notifier).changePhotoJpgBytes(null);
+
+    showSnackBar(
+      context,
+      ref,
+      content: const Text('投稿しました'),
+      duration: const Duration(seconds: 6),
+      action: SnackBarAction(
+        label: '取消',
+        onPressed: () async {
+          await undo(context, docId: docId);
+        },
+      ),
+    );
+  }
+
+  Future<void> undo(BuildContext context, {required String docId}) async {
+    await ref.read(articleRepositoryProvider).delete(docId);
+    showSnackBar(
+      context,
+      ref,
+      content: Text('投稿を取り消しました'),
+      duration: Duration(seconds: 3),
+    );
+  }
+
+  Future<void> deleteArticle(BuildContext context,
+      {required String docId}) async {
+    await ref.read(articleRepositoryProvider).delete(docId);
+    showSnackBar(
+      context,
+      ref,
+      content: Text('削除しました'),
+      duration: Duration(seconds: 3),
+    );
+  }
+
+  Future<void> preparePhoto(BuildContext context) async {
+    final picker = ImagePicker();
+    final result = await picker.pickImage(source: ImageSource.gallery);
+    if (result == null) {
+      return;
+    }
+    final mimeType = result.mimeType;
+    if (mimeType == null) {
+      showSnackBar(
+        context,
+        ref,
+        content: Text('ファイルの読み込みに失敗しました。'),
+      );
+      return;
+    }
+    final resultBytes = await result.readAsBytes();
+    final tempImage = await decodeBytes(mimeType: mimeType, bytes: resultBytes);
+    if (tempImage == null) {
+      showSnackBar(
+        context,
+        ref,
+        content: Text('ファイルの読み込みに失敗しました。'),
+      );
+      return;
+    }
+    final resized = img.copyResize(tempImage, width: min(tempImage.width, 500));
+    final bytes = img.encodeJpg(resized, quality: 80).buffer.asUint8List();
+    ref.read(bbsFormProvider.notifier).changePhotoJpgBytes(bytes);
+  }
+
+  void showProfile(String userId) {
+    ref.read(selectedUserIdProvider.notifier).select(userId);
+  }
+}
---
+++ lib/ui/pages/bbs/bbs_controller.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bbs_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(BbsController)
+final bbsControllerProvider = BbsControllerProvider._();
+
+final class BbsControllerProvider
+    extends $NotifierProvider<BbsController, BbsController> {
+  BbsControllerProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'bbsControllerProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$bbsControllerHash();
+
+  @$internal
+  @override
+  BbsController create() => BbsController();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(BbsController value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<BbsController>(value),
+    );
+  }
+}
+
+String _$bbsControllerHash() => r'1e3d08a799c7431d5caac8b3b49eb4c1a7f00f3a';
+
+abstract class _$BbsController extends $Notifier<BbsController> {
+  BbsController build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<BbsController, BbsController>;
+    final element = ref.element as $ClassProviderElement<
+        AnyNotifier<BbsController, BbsController>,
+        BbsController,
+        Object?,
+        Object?>;
+    element.handleCreate(ref, build);
+  }
+}
